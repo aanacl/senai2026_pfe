0ab89121299799217d8c6b1fa38f0d33ed8149b1
aanacl <ana.c.silva659@aluno.senai.br>
2026-02-12 11:08:06 -0300

    Add files via upload

--- exercicio1.js
+++ exercicio1.js
@@ -1,8 +1,20 @@
-function calcularPontuacaoFinal(pontosBase) {
-    let dividido = pontosBase / 2;
-    let final = dividido ** 2;
+​
+const corpo=document.querySelector('body');
+const titulo=document.querySelector('.titulo');
+const container=document.querySelector('#container');
+const corpo2=document.createElement('img')
 
-    console.log("Pontuação final:", final);
-}
+corpo.style.backgroundColor='#821f41';
+corpo.style.color='black';
+titulo.textContent='ana';
+container.style.width='94%';
+container.style.alingItems='center';
+container.style.margin='0 auto';
+container.style.backgroundColor='#9e6292';
 
-calcularPontuacaoFinal(10);
+let uc='<h2> Programação front-end!!! </h2>';
+container.innerHTML+=uc;
+
+imagem.innerHTML += '<img src="https://wallpapers.com/images/hd/spider-man-pictures-50hezdq7ovyrql7i.jpg" alt="Imagem de JavaScript DOM">';
+
+imagem.style.textAlign = 'center';
