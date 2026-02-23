b722e2a229eb7feff37d7e8248651194a15fab8a
aanacl <ana.c.silva659@aluno.senai.br>
2026-02-23 11:08:59 -0300

    Add files via upload

--- exercicio1.js
+++ exercicio1.js
@@ -1,20 +1,12 @@
-​
-const corpo=document.querySelector('body');
-const titulo=document.querySelector('.titulo');
-const container=document.querySelector('#container');
-const corpo2=document.createElement('img')
+class Livros{
+//atributos
+nome= 'os 7 maridos de evelyn hugo';
+genero= 'romance';
+preco= 30;
+autora= 'taylor jenkis';
+data= '13 junho'; 
+ano='2017';
+atriz='evelyn hugo';
+editora='paralela';
 
-corpo.style.backgroundColor='#821f41';
-corpo.style.color='black';
-titulo.textContent='ana';
-container.style.width='94%';
-container.style.alingItems='center';
-container.style.margin='0 auto';
-container.style.backgroundColor='#9e6292';
-
-let uc='<h2> Programação front-end!!! </h2>';
-container.innerHTML+=uc;
-
-imagem.innerHTML += '<img src="https://wallpapers.com/images/hd/spider-man-pictures-50hezdq7ovyrql7i.jpg" alt="Imagem de JavaScript DOM">';
-
-imagem.style.textAlign = 'center';
+};
--- exercicio2.js
+++ exercicio2.js
@@ -1,14 +1,44 @@
-const prompt = require('prompt-sync')()
-function Usuario() {
-    let id = Number(prompt(" sua idade "));
-
-    if (id >= 18) {
-        console.log(" pegar livros e tem idade suficiente");
-    } else if (id >= 16) {
-        console.log("pode pegar os livros e é menor de idade");
-    } else {
-        console.log("não pode pegar livros");
-    }
+class bicleta{
+//atributos
+#modelo= 'explorer pro';
+#marca= 'caloi';
+#cor= 'roxa';
+#velocidademax= '35km';
+//metodos getter e setter
+setModelo(valor){
+    this.#modelo = valor;
+} 
+getModelo(){
+    return this.#modelo;
 }
-
-Usuario();
+setMarca(valor){
+    this.#marca = valor;
+} 
+getMarca(){
+    return this.#marca;
+}
+setCor(valor){
+    this.#cor = valor;
+} 
+getCor(){
+    return this.#cor;
+}
+setVelocidademax(valor){
+    if(valor > 35){
+        console.log('nao é possivel inserir acima de 35');
+    }
+    this.#velocidademax = valor;
+} 
+getVelocidademax(){
+    return this.#velocidademax;
+}
+}
+const caloi = new bicleta();
+caloi.setMarca('caloi')
+console.log(caloi.getMarca());
+caloi.setCor('roxa')
+console.log(caloi.getCor());
+caloi.setModelo('explorer pro')
+console.log(caloi.getModelo());
+caloi.setVelocidademax(36)
+console.log(caloi.getVelocidademax());
