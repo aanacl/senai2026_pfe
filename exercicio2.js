52e9b2091fce9cbfd9eda301da1465c8f743442f
aanacl <ana.c.silva659@aluno.senai.br>
2026-02-26 10:48:55 -0300

    Add files via upload

--- exercicio2.js
+++ exercicio2.js
@@ -1,44 +1,76 @@
-class bicleta{
-//atributos
-#modelo= 'explorer pro';
-#marca= 'caloi';
-#cor= 'roxa';
-#velocidademax= '35km';
-//metodos getter e setter
-setModelo(valor){
-    this.#modelo = valor;
-} 
-getModelo(){
-    return this.#modelo;
-}
-setMarca(valor){
-    this.#marca = valor;
-} 
-getMarca(){
-    return this.#marca;
-}
-setCor(valor){
-    this.#cor = valor;
-} 
-getCor(){
-    return this.#cor;
-}
-setVelocidademax(valor){
-    if(valor > 35){
-        console.log('nao é possivel inserir acima de 35');
+class Jogador {
+    #nome;
+    #numero;
+
+    constructor(nome, numero) {
+        this.#nome = nome;
+        this.#numero = numero;
     }
-    this.#velocidademax = valor;
-} 
-getVelocidademax(){
-    return this.#velocidademax;
+
+    treinar() {
+        return '3 vezes na semana';
+    }
+
+    getNome() {
+        return this.#nome;
+    }
+
+    getNumero() {
+        return this.#numero;
+    }
 }
+
+
+class JogadorFutebolAmericano extends Jogador {
+    #listaJogadas;
+    #jardasConquistadas;
+
+    constructor(nome, numero, listaJogadas, jardasConquistadas) {
+        super(nome, numero);
+        this.#listaJogadas = listaJogadas;
+        this.#jardasConquistadas = jardasConquistadas;
+    }
+
+    fazerTouchDown() {
+        return "O jogador número " + super.getNumero() + " fez touchdown";
+    }
+
+    bloquear() {
+        return "O jogador realizou um bloqueio";
+    }
+
+    correrJardas(valor) {
+        this.#jardasConquistadas += valor;
+        return this.#jardasConquistadas;
+    }
 }
-const caloi = new bicleta();
-caloi.setMarca('caloi')
-console.log(caloi.getMarca());
-caloi.setCor('roxa')
-console.log(caloi.getCor());
-caloi.setModelo('explorer pro')
-console.log(caloi.getModelo());
-caloi.setVelocidademax(36)
-console.log(caloi.getVelocidademax());
+
+
+class JogadorBasquete extends Jogador {
+    #alturaSalto;
+    #totalCestas;
+
+    constructor(nome, numero, alturaSalto, totalCestas) {
+        super(nome, numero);
+        this.#alturaSalto = alturaSalto;
+        this.#totalCestas = totalCestas;
+    }
+
+    arremessar() {
+        return "O jogador nome " + super.getNome() + 
+               " e número " + super.getNumero() + 
+               " arremessou a bola";
+    }
+
+    fazerEnterrada() {
+        this.#totalCestas++;
+        return "Enterrada realizada!";
+    }
+}
+
+
+const jogadorFA = new JogadorFutebolAmericano("Carlos", 10, [], 120);
+console.log(jogadorFA.fazerTouchDown());
+
+const jogadorBasquete = new JogadorBasquete("Lucas", 23, 80, 15);
+console.log(jogadorBasquete.arremessar());
